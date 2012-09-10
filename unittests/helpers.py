cfe5b6f4a48d9c1bae4961199fb738c14922cd60
Klaus Greff <klaus@idsia.ch>
2012-09-10 10:05:26 +0200

    added unittests package

---
+++ unittests/helpers.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# coding: utf-8
+from __future__ import division, unicode_literals, print_function
+
+from numpy.testing import assert_almost_equal, assert_equal
+from nose.tools import istest, nottest, with_setup, assert_true
