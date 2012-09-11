47cbe4ff0c129ab6ddf851d93fb489da90caf182
Klaus Greff <klaus@idsia.ch>
2012-09-11 19:33:38 +0200

    refactored the unittests for FANN

--- unittests/test_fann.py
+++ unittests/test_fann.py
@@ -73,45 +73,34 @@ def test_FANN_with_bias_error_multisample():
 def test_FANN_gradient_single_sample():
     fc = FullConnection(4, 1)
     nn = FANN([fc])
+    theta = np.random.randn(nn.get_param_dim())
     for x, t in zip(X, T) :
         grad_c = nn.calculate_gradient(theta, x, t)
-        grad_e = approx_fprime(theta, nn.calculate_error, 1e-7, x, t)
-        assert_almost_equal(grad_c, grad_e)
-    for x in X :
-        grad_c = nn.calculate_gradient(theta, x, 0)
-        grad_e = approx_fprime(theta, nn.calculate_error, 1e-7, x, 0)
+        grad_e = approx_fprime(theta, nn.calculate_error, 1e-8, x, t)
         assert_almost_equal(grad_c, grad_e)
 
 def test_FANN_with_bias_gradient_single_sample():
     fc = FullConnectionWithBias(3, 1)
     nn = FANN([fc])
+    theta = np.random.randn(nn.get_param_dim())
     for x, t in zip(X_nb, T) :
         grad_c = nn.calculate_gradient(theta, x, t)
-        grad_e = approx_fprime(theta, nn.calculate_error, 1e-7, x, t)
-        assert_almost_equal(grad_c, grad_e)
-    for x in X_nb :
-        grad_c = nn.calculate_gradient(theta, x, 0)
-        grad_e = approx_fprime(theta, nn.calculate_error, 1e-7, x, 0)
+        grad_e = approx_fprime(theta, nn.calculate_error, 1e-8, x, t)
         assert_almost_equal(grad_c, grad_e)
 
 def test_FANN_gradient_multisample():
     fc = FullConnection(4, 1)
     nn = FANN([fc])
+    theta = np.random.randn(nn.get_param_dim())
     grad_c = nn.calculate_gradient(theta, X, T)
     grad_e = approx_fprime(theta, nn.calculate_error, 1e-8, X, T)
-    assert_almost_equal(grad_c, grad_e)
-
-    grad_c = nn.calculate_gradient(theta, X, np.zeros_like(T))
-    grad_e = approx_fprime(theta, nn.calculate_error, 1e-8, X, np.zeros_like(T))
     assert_almost_equal(grad_c, grad_e)
 
 def test_FANN_with_bias_gradient_multisample():
     fc = FullConnectionWithBias(3, 1)
     nn = FANN([fc])
+    theta = np.random.randn(nn.get_param_dim())
     grad_c = nn.calculate_gradient(theta, X_nb, T)
     grad_e = approx_fprime(theta, nn.calculate_error, 1e-8, X_nb, T)
     assert_almost_equal(grad_c, grad_e)
 
-    grad_c = nn.calculate_gradient(theta, X_nb, np.zeros_like(T))
-    grad_e = approx_fprime(theta, nn.calculate_error, 1e-8, X_nb, np.zeros_like(T))
-    assert_almost_equal(grad_c, grad_e)
