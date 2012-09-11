929f8196e9518228413372f362f7f68436114fd9
Klaus Greff <klaus@idsia.ch>
2012-09-11 17:50:51 +0200

    fixed calculate_gradient and refactored FANN

--- integration_tests/test_toy_convergence.py
+++ integration_tests/test_toy_convergence.py
@@ -2,36 +2,41 @@
 # coding: utf-8
 from __future__ import division, unicode_literals, print_function
 import numpy as np
-from datasets import load_and, generate_majority_vote
+from datasets import load_and, generate_majority_vote, load_xor
+from neural_nets.connections import FullConnectionWithBias, FullConnection
 from neural_nets.fann import FANN
 from unittests.helpers import assert_less
 
 def test_FANN_converges_on_and_problem():
-    nn = FANN(2, 1)
+    fc = FullConnection(2, 1)
+    nn = FANN([fc])
     and_ = load_and()
     theta = np.array([-0.1, 0.1])
     for i in range(100):
-        g, _, _ = nn.calculate_gradient(theta, and_.data, and_.target)
+        g = nn.calculate_gradient(theta, and_.data, and_.target)
         theta -= g * 1
     error = nn.calculate_error(theta, and_.data, and_.target)
     assert_less(error,  0.2)
 
 def test_FANN_converges_on_xor_problem():
-    nn = FANN(2, 1)
-    xor = load_and()
-    theta = np.array([-0.1, 0.1])
-    for i in range(100):
-        g, _, _ = nn.calculate_gradient(theta, xor.data, xor.target)
+    fc0 = FullConnection(2, 2)
+    fc1 = FullConnection(2, 1)
+    nn = FANN([fc0, fc1])
+    xor = load_xor()
+    theta = np.random.randn(len(nn))
+    for i in range(1000):
+        g = nn.calculate_gradient(theta, xor.data, xor.target)
         theta -= g * 1
     error = nn.calculate_error(theta, xor.data, xor.target)
     assert_less(error,  0.2)
 
 def test_FANN_converges_on_vote_problem():
-    nn = FANN(9, 1, include_bias=True)
+    fc = FullConnectionWithBias(9, 1)
+    nn = FANN([fc])
     vote = generate_majority_vote()
     theta = np.zeros((10,))
     for i in range(500):
-        g, _, _ = nn.calculate_gradient(theta, vote.data, vote.target)
+        g = nn.calculate_gradient(theta, vote.data, vote.target)
         theta -= g * 1
     error = nn.calculate_error(theta, vote.data, vote.target)
     assert_less(error,  0.2)
--- neural_nets/fann.py
+++ neural_nets/fann.py
@@ -14,15 +14,28 @@ class FANN(object):
         self.output_size = layers[-1].output_dim
         self.layers = layers
 
+    def __len__(self):
+        dim = 0
+        for l in self.layers:
+            dim += len(l)
+        return dim
+
+    def slice_theta(self, theta):
+        slices = []
+        theta_offset = 0
+        for layer in self.layers:
+            param_dim = len(layer)
+            slices.append(theta[theta_offset:theta_offset + param_dim])
+            theta_offset += param_dim
+        return slices
+
     def forward_pass(self, theta, X):
         """
         Using the parameters theta as the weights evaluate this feed-forward
         neural network on the data X returning only the outcome.
         """
-        activation = X
-        for layer in self.layers:
-            activation = sigmoid(layer.forward_pass(theta, activation))
-        return activation
+        # NOTE: This method might be optimized
+        return self.full_forward_pass(theta, X)[-1]
 
     def full_forward_pass(self, theta, X):
         """
@@ -30,8 +43,8 @@ class FANN(object):
         neural network on the data X returning the full list of activations.
         """
         activations = [X]
-        for layer in self.layers:
-            activations.append(sigmoid(layer.forward_pass(theta, activations[-1])))
+        for layer, th in zip(self.layers, self.slice_theta(theta)):
+            activations.append(sigmoid(layer.forward_pass(th, activations[-1])))
         return activations
 
     def calculate_error(self, theta, X, T):
@@ -43,12 +56,13 @@ class FANN(object):
         Y = activations[-1]
         delta = (T - Y) * Y * (1 - Y) #sigmoid
         grad_theta = self.layers[-1].calculate_gradient(activations[-2], delta).reshape(-1)
+        theta_slices = self.slice_theta(theta)
         for i in range(len(self.layers) - 1, 0, -1):
             a = activations[i]
             layer = self.layers[i]
-            delta = layer.pass_down(delta) * a * (1 - a) #sigmoid
+            delta = layer.backward_pass(theta_slices[i], delta) * a * (1 - a) #sigmoid
             grad = layer.calculate_gradient(a, delta)
-            grad_theta = np.hstack((grad_theta, grad.reshape(-1)))
+            grad_theta = np.hstack((grad.reshape(-1), grad_theta))
         return grad_theta
 
 
