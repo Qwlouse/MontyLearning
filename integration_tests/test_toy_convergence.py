65ca266c156c14dd86ea90074709aab3c6ca5e89
Klaus Greff <klaus@idsia.ch>
2012-09-11 10:01:24 +0200

    added xor problem to  integration tests

--- integration_tests/test_toy_convergence.py
+++ integration_tests/test_toy_convergence.py
@@ -4,6 +4,7 @@ from __future__ import division, unicode_literals, print_function
 import numpy as np
 from datasets import load_and
 from neural_nets.fann import FANN
+from unittests.helpers import assert_less
 
 def test_FANN_converges_on_and_problem():
     nn = FANN(2, 1)
@@ -13,4 +14,14 @@ def test_FANN_converges_on_and_problem():
         g, _, _ = nn.calculate_gradient(theta, and_.data, and_.target)
         theta -= g * 1
     error = nn.calculate_error(theta, and_.data, and_.target)
-    assert error < 0.2
+    assert_less(error,  0.2)
+
+def test_FANN_converges_on_xor_problem():
+    nn = FANN(2, 1)
+    xor = load_and()
+    theta = np.array([-0.1, 0.1])
+    for i in range(100):
+        g, _, _ = nn.calculate_gradient(theta, xor.data, xor.target)
+        theta -= g * 1
+    error = nn.calculate_error(theta, xor.data, xor.target)
+    assert_less(error,  0.2)
--- unittests/helpers.py
+++ unittests/helpers.py
@@ -3,6 +3,7 @@
 from __future__ import division, unicode_literals, print_function
 
 from numpy.testing import assert_almost_equal, assert_equal
+from numpy.testing import assert_array_less as assert_less
 from nose.tools import istest, nottest, with_setup, assert_true
 
 # Use the same flag as unittest itself to prevent descent into these functions:
