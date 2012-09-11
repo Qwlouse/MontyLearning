5c488611d9cb8f963bf90d74bbf9d7b6229db55c
Klaus Greff <klaus@idsia.ch>
2012-09-11 19:53:11 +0200

    refactored FANN a bit

--- neural_nets/fann.py
+++ neural_nets/fann.py
@@ -31,32 +31,33 @@ class FANN(object):
 
     def forward_pass(self, theta, X):
         # NOTE: This method might be optimized
-        return self.full_forward_pass(theta, X)[-1]
+        _, activations = self.full_forward_pass(theta, X)
+        return activations[-1]
 
     def full_forward_pass(self, theta, X):
         """
         Using the parameters theta as the weights evaluate this feed-forward
-        neural network on the data X returning the full list of activations.
+        neural network on the data X returning the sliced theta and full list of activations.
         """
         activations = [X]
-        for layer, th in zip(self.layers, self.slice_theta(theta)):
+        sliced_theta = self.slice_theta(theta)
+        for layer, th in zip(self.layers,sliced_theta):
             activations.append(sigmoid(layer.forward_pass(th, activations[-1])))
-        return activations
+        return sliced_theta, activations
 
     def calculate_error(self, theta, X, T):
         Y = self.forward_pass(theta, X)
         return np.sum(0.5 * (T - Y)**2)
 
     def calculate_gradient(self, theta, X, T):
-        activations = self.full_forward_pass(theta, X)
+        sliced_theta, activations = self.full_forward_pass(theta, X)
         Y = activations[-1]
         delta = (T - Y) * Y * (1 - Y) #sigmoid
         grad_theta = self.layers[-1].calculate_gradient(activations[-2], delta).reshape(-1)
-        theta_slices = self.slice_theta(theta)
         for i in range(len(self.layers) - 1, 0, -1):
             a = activations[i]
             layer = self.layers[i]
-            delta = layer.backward_pass(theta_slices[i], delta) * a * (1 - a) #sigmoid
+            delta = layer.backward_pass(sliced_theta[i], delta) * a * (1 - a) #sigmoid
             grad = self.layers[i-1].calculate_gradient(activations[i-1], delta)
             grad_theta = np.hstack((grad.reshape(-1), grad_theta))
         return grad_theta
