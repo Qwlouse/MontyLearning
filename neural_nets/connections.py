e61beb5d1f78741e279772815278b6b31a358c92
Klaus Greff <klaus@idsia.ch>
2012-09-11 20:25:49 +0200

    refactored FullConnectionWithBias to inherit from FullConnection

--- neural_nets/connections.py
+++ neural_nets/connections.py
@@ -35,14 +35,10 @@ class FullConnection(object):
         X = np.atleast_2d(X)
         return -X.T.dot(delta)
 
-class FullConnectionWithBias(object):
+class FullConnectionWithBias(FullConnection):
     """
     Linear feed-forward full connection WITH bias.
     """
-    def __init__(self, input_dim, output_dim):
-        self.input_dim = input_dim
-        self.output_dim = output_dim
-
     def get_param_dim(self):
         """
         Return the dimension of the parameter-space.
@@ -53,14 +49,10 @@ class FullConnectionWithBias(object):
         return theta.reshape(self.input_dim + 1, self.output_dim)
 
     def forward_pass(self, theta, X):
-        W = self.unpackTheta(theta)
-        X = add_bias(np.atleast_2d(X))
-        return X.dot(W)
+        return FullConnection.forward_pass(self, theta, add_bias(X))
 
     def backward_pass(self, theta, Y):
-        W = self.unpackTheta(theta)
-        return Y.dot(W.T)[:,:-1]
+        return FullConnection.backward_pass(self, theta, Y)[:,:-1]
 
     def calculate_gradient(self, X, delta):
-        X = add_bias(np.atleast_2d(X))
-        return -X.T.dot(delta)
+        return FullConnection.calculate_gradient(self, add_bias(X), delta)
