1805fc84ba50a055b01bea354ef7040efb18ce40
Qwlouse <klaus.greff@gmx.net>
2012-09-28 01:33:19 +0200

    wrapped assert_true to keep IDE errors in helpers file

--- unittests/helpers.py
+++ unittests/helpers.py
@@ -4,7 +4,11 @@ from __future__ import division, unicode_literals, print_function
 
 from numpy.testing import assert_almost_equal, assert_equal, assert_allclose
 from numpy.testing import assert_array_less as assert_less
-from nose.tools import istest, nottest, with_setup, assert_true
+from nose.tools import istest, nottest, with_setup
+from nose.tools import assert_true as _assert_true
+
+def assert_true(*args, **kwargs):
+    _assert_true(*args, **kwargs)
 
 # Use the same flag as unittest itself to prevent descent into these functions:
 __unittest = 1
