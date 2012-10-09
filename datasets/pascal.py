89f65793f414ea48716cfc95e48e3cbc16a38956
Klaus Greff <klaus@idsia.ch>
2012-10-09 14:12:35 +0200

    class matrix methods to select only a subset of images

--- datasets/pascal.py
+++ datasets/pascal.py
@@ -6,6 +6,8 @@ Methods to load and edit the Pascal VOC dataset.
 from __future__ import division, print_function, unicode_literals
 from config import PASCAL_PATH
 import os.path as path
+import numpy as np
+from scipy.misc import imread
 
 IMAGE_SET_DIR          = path.join(PASCAL_PATH, "ImageSets")
 SEGMENTATION_SET_DIR   = path.join(PASCAL_PATH, "ImageSets/Segmentation")
@@ -40,3 +42,31 @@ get_seg_trainval_class_label_files = lambda : get_image_paths_for(SEGMENTATION_C
 get_seg_train_obj_label_files    = lambda : get_image_paths_for(SEGMENTATION_OBJ_DIR, seg_train_set_file, ".png")
 get_seg_val_obj_label_files      = lambda : get_image_paths_for(SEGMENTATION_OBJ_DIR, seg_val_set_file, ".png")
 get_seg_trainval_obj_label_files = lambda : get_image_paths_for(SEGMENTATION_OBJ_DIR, seg_trainval_set_file, ".png")
+
+
+def get_classes_matrix(label_img_path_iter, nr_of_classes = 22):
+    """
+    construct a matrix with one row for each image and one column for each class including BACKGROUND and VOID.
+    Each entry is 1 if that image contains that class and 0 otherwise.
+    """
+    classes = []
+    for img_path in label_img_path_iter:
+        img = imread(img_path)
+        unique = np.unique(img)
+        prevalent_classes = np.zeros(nr_of_classes)
+        for u in unique:
+            u = min(u, nr_of_classes-1)
+            prevalent_classes[u] += 1
+        classes.append(prevalent_classes)
+    return np.array(classes)
+
+def get_idx_containing_only(class_matrix, allowed_classes):
+    """
+    Given a classes_matrix determin the indices of those images containing only certain classes.
+    """
+    classes = class_matrix.copy()
+    for i in range(class_matrix.shape[1]):
+        if i in allowed_classes:
+            classes[:,i] = 0
+
+    return np.nonzero(classes.sum(1) == 0)
