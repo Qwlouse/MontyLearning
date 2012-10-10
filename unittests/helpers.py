f65d838a32d54ecc8087d04316f2abf8a249cd4a
Klaus Greff <klaus@idsia.ch>
2012-10-10 14:28:16 +0200

    defined own assert_true and assert_not_equal in helpers to prevent IDE import error from propagating

--- unittests/helpers.py
+++ unittests/helpers.py
@@ -5,8 +5,15 @@ from __future__ import division, unicode_literals, print_function
 from numpy.testing import assert_almost_equal, assert_equal, assert_allclose
 from numpy.testing import assert_array_less as assert_less
 from nose.tools import istest, nottest, with_setup
-from nose.tools import assert_true, assert_not_equal
+from nose.tools import assert_true as _assert_true
+from nose.tools import assert_not_equal as _assert_not_equal
+from nose.tools import raises
 
+def assert_true(expr, msg=None):
+    _assert_true(expr, msg)
+
+def assert_not_equal(first, second, msg=None):
+    _assert_not_equal(first, second, msg)
 
 # Use the same flag as unittest itself to prevent descent into these functions:
 __unittest = 1
