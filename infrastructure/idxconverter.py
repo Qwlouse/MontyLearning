8c4ef9d13ccc0a2c5aa39df1b6e698ccd74cd540
Klaus Greff <klaus@idsia.ch>
2012-10-11 18:51:18 +0200

    extended the idx-converter

--- infrastructure/idxconverter.py
+++ infrastructure/idxconverter.py
@@ -20,6 +20,7 @@ REVERSE_TYPE_DICT = {
     np.zeros(1, dtype=np.int8).dtype    : b"\x09",
     np.zeros(1, dtype=np.int16).dtype   : b"\x0B",
     np.zeros(1, dtype=np.int32).dtype   : b"\x0C",
+    np.zeros(1, dtype=np.uint32).dtype  : b"\x0C",
     np.zeros(1, dtype=np.float32).dtype : b"\x0D",
     np.zeros(1, dtype=np.float64).dtype : b"\x0E"
 }
@@ -27,7 +28,7 @@ REVERSE_TYPE_DICT = {
 
 
 
-def open_idx_file(filename):
+def open_idx_file(filename, byteswap=True):
     with open(filename, 'rb') as f:
         zero = np.fromfile(f, np.int16, 1).byteswap()[0]
         assert zero == 0, "File should start with two zero-bytes but was %s."%hex(zero)
@@ -36,12 +37,20 @@ def open_idx_file(filename):
         dim_count = np.fromfile(f, np.uint8, 1)[0]
         shape = np.fromfile(f, np.int32, dim_count).byteswap()
         size = reduce(np.multiply, shape)
-        return np.fromfile(f, TYPE_DICT[type_code], size).byteswap().reshape(shape)
+        if byteswap:
+            return np.fromfile(f, TYPE_DICT[type_code], size).byteswap().reshape(shape)
+        else:
+            return np.fromfile(f, TYPE_DICT[type_code], size).reshape(shape)
 
-def write_idx_file(filename, A):
+def write_idx_file(filename, A, shape=None, byteswap=True):
     with open(filename, 'wb') as f:
         f.write(b"\x00\x00") # zeros
         f.write(REVERSE_TYPE_DICT[A.dtype]) # type
         f.write(b"%c"%len(A.shape)) # nr of dims
-        np.array(A.shape, dtype=np.int32).byteswap(True).tofile(f) # shape
-        A.byteswap(False).tofile(f) # numbers
+        if shape is None:
+            shape = A.shape
+        np.array(shape, dtype=np.int32).byteswap(True).tofile(f) # shape
+        if byteswap :
+            A.byteswap(False).tofile(f) # numbers
+        else :
+            A.tofile(f) # numbers
