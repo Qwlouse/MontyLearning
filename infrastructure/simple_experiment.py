b1aa935faa5d30b548924e7b53660a09307ab4c6
Qwlouse <klaus.greff@gmx.net>
2012-10-13 22:08:56 +0200

    very simple experiment as a test case for caching

---
+++ infrastructure/simple_experiment.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# coding=utf-8
+"""
+# This is a demo experiment to explore the workflow.
+# I'll (mis)use this docstring to give a description and also
+# contain the configuration file.
+
+seed = 1234567890
+n = 3
+m = 4
+bias = 1
+
+"""
+from __future__ import division, print_function, unicode_literals
+import numpy as np
+import time
+from experiment import Experiment
+
+ex = Experiment(__doc__)
+
+@ex.full_stage
+def create_data(n, m, bias, logger):
+    logger.info("Creating {}x{} array with bias {}.".format(n,m, bias))
+    A = np.arange(n*m).reshape(n,m) + bias
+    return A
+
+@ex.full_stage
+def square(A, logger):
+    logger.info("Squaring {}x{} array.".format(A.shape[0], A.shape[1]))
+    return A * A
+
+@ex.main
+def main():
+    A = create_data()
+    B = square(A)
+
+    time.sleep(.1)
+    print(B)
+
